5f6202185be8fa0dea015574288f8cfa82fa8b60
William Silva <euwilliamsantana@gmail.com>
2022-09-16 18:38:26 -0300

    Estrutura completa

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,5 @@
 import "./styles/main.css"
+import { MagnifyingGlassPlus} from "phosphor-react"
 import logoImg from "./assets/logo-nlw.svg"
 
 
@@ -28,7 +29,7 @@ function App() {
 
         <div className="w-full pt-16 px-4 pb-4 bg-nlw-game absolute bottom-0 left-0 right-0">
 
-          <strong className="font-bold text-white block">Dota 2</strong>
+          <strong className="font-bold text-white block"></strong>
           <span className="text-zinc-300 block text-sm">4 anúncios</span>
         </div>
       </a>
@@ -76,6 +77,26 @@ function App() {
      
 
     </div>
+
+    <div className="self-stretch mt-8 rounded-lg bg-nlw-linear pt-1 ">
+
+      <div className="bg-[#2A2634] px-8 py-4 flex justify-between items-center">
+    
+      <div>
+          <strong className="text-white font-black text-2xl block">Não encontrou seu duo?</strong>
+          <span className="text-zinc-400 block">Publique um anúncio para encontrar novos players!</span>
+
+      </div>
+
+      <button className="py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white font-medium rounded-md flex items-center gap-3">
+        <MagnifyingGlassPlus size={24}/>
+        Publicar anúncio
+      </button>
+
+      </div>
+
+    </div>
+
 
 
     </div>
