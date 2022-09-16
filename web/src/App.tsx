904627189982147598c2c46d0968147db14311d2
William Silva <euwilliamsantana@gmail.com>
2022-09-16 18:02:30 -0300

    Create list games

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -11,6 +11,73 @@ function App() {
       Seu <span className="text-transparent bg-nlw-linear bg-clip-text ">duo</span> está aqui.
     </h1>
 
+    <div className="grid grid-cols-6 gap-6 mt-16">
+
+      <a href="" className="relative rounded-lg overflow-hidden">
+        <img src="./game-1.png" alt="" />
+
+        <div className="w-full pt-16 px-4 pb-4 bg-nlw-game absolute bottom-0 left-0 right-0">
+
+          <strong className="font-bold text-white block">League of Legends</strong>
+          <span className="text-zinc-300 block text-sm">4 anúncios</span>
+        </div>
+      </a>
+
+      <a href="" className="relative rounded-lg overflow-hidden">
+        <img src="./game-2.png" alt="" />
+
+        <div className="w-full pt-16 px-4 pb-4 bg-nlw-game absolute bottom-0 left-0 right-0">
+
+          <strong className="font-bold text-white block">Dota 2</strong>
+          <span className="text-zinc-300 block text-sm">4 anúncios</span>
+        </div>
+      </a>
+
+      <a href="" className="relative rounded-lg overflow-hidden">
+        <img src="./game-3.png" alt="" />
+
+        <div className="w-full pt-16 px-4 pb-4 bg-nlw-game absolute bottom-0 left-0 right-0">
+
+          <strong className="font-bold text-white block">Counter Strike</strong>
+          <span className="text-zinc-300 block text-sm">4 anúncios</span>
+        </div>
+      </a>
+
+      <a href="" className="relative rounded-lg overflow-hidden">
+        <img src="./game-4.png" alt="" />
+
+        <div className="w-full pt-16 px-4 pb-4 bg-nlw-game absolute bottom-0 left-0 right-0">
+
+          <strong className="font-bold text-white block">Apex Legends</strong>
+          <span className="text-zinc-300 block text-sm">4 anúncios</span>
+        </div>
+      </a>
+
+      <a href="" className="relative rounded-lg overflow-hidden">
+        <img src="./game-5.png" alt="" />
+
+        <div className="w-full pt-16 px-4 pb-4 bg-nlw-game absolute bottom-0 left-0 right-0">
+
+          <strong className="font-bold text-white block">Fortnite</strong>
+          <span className="text-zinc-300 block text-sm">4 anúncios</span>
+        </div>
+      </a>
+
+      <a href="" className="relative rounded-lg overflow-hidden">
+        <img src="./game-6.png" alt="" />
+
+        <div className="w-full pt-16 px-4 pb-4 bg-nlw-game absolute bottom-0 left-0 right-0">
+
+          <strong className="font-bold text-white block">World of Warcraft</strong>
+          <span className="text-zinc-300 block text-sm">4 anúncios</span>
+        </div>
+      </a>
+
+     
+
+    </div>
+
+
     </div>
   )
  
--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -6,7 +6,9 @@ module.exports = {
       backgroundImage: {
         galaxy: "url('./public/background-galaxy.png')",
         'nlw-linear':
-          'linear-gradient(89.86deg, #9572FC 35.08%, #43E7AD 23.94%, #E1D55D 44.57%);'
+          'linear-gradient(89.86deg, #9572FC 35.08%, #43E7AD 23.94%, #E1D55D 44.57%);',
+        'nlw-game':
+          'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%);'
       }
     }
   },
