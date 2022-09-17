02dc411b83c47b85f5e0e404dc88c4231ea063de
William Silva <euwilliamsantana@gmail.com>
2022-09-17 12:42:19 -0300

    Banco de dados, Back - ok

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,10 +3,25 @@ import express from "express"
 
 const app = express()
 
+app.get("/games", (request, response)=>{
 
-app.get("/", (request, response)=>{
+  response.json([])
+
+})
+
+app.post("/ads", (request, response)=>{
 
   response.json({message: "Hello WorldS"})
+
+})
+app.get("/games/:id/ads", (request, response)=>{
+    const gameId = request.params.id
+    response.send(gameId)
+
+})
+app.get("/ads/:id/discord", (request, response)=>{
+    const adId = request.params.id
+    response.json([])
 
 })
 
